90e7250e63f49c5e26b81d75a3752cfa954dc4fd
The Other Promise <150410378+noe-bee@users.noreply.github.com>
2024-08-21 20:42:39 -0700

    added content to cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -13,41 +13,37 @@ function MultiCard() {
         <Card.Body style={{border: '2px dotted'}}>
           <Card.Title className="text-primary">Ease</Card.Title>
           <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
+            A fully functioning chat app with a seamless UI which allows the user to chat with others. After creating an account, users can add others as friends!
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <a className="text-primary" href="#">Click For Live App!</a>
+          <a className="text-primary" href="https://chat-test-bquw.onrender.com">Click For Live App!</a>
         </Card.Footer>
       </Card>
 
       <Card style={{ margin: '2rem' }}>
         <Card.Img variant="top" src="src/assets/placeholder-500x500.jpg" />
         <Card.Body style={{border: '2px dotted'}}>
-          <Card.Title className="text-primary">Card title</Card.Title>
+          <Card.Title className="text-primary">Weather Dashbaord</Card.Title>
           <Card.Text>
-            This card has supporting text below as a natural lead-in to
-            additional content.{' '}
+           A weather dashboard app featuring the use of an api for accurate weather data anywhere you can think of for up to the next 5 days!
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-          <a className="text-primary" href="#">Click For Live App!</a>
+          <a className="text-primary" href="https://noe-bee.github.io/weather-dashboard/">Click For Live App!</a>
         </Card.Footer>
       </Card>
 
       <Card style={{ margin: '2rem' }}>
         <Card.Img variant="top" src="src/assets/placeholder-500x500.jpg" />
         <Card.Body style={{border: '2px dotted'}}>
-          <Card.Title className="text-primary">Card title</Card.Title>
+          <Card.Title className="text-primary">WorkDay Scheduler</Card.Title>
           <Card.Text>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This card has even longer content than the
-            first to show that equal height action.
+            A simple, yet useful app that allows you to note your tasks for a typical 9-5 workday. This app saves your past, current, and future tasks for the day!
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-        <a className="text-primary" href="#">Click For Live App!</a>
+        <a className="text-primary" href="https://noe-bee.github.io/workday-scheduler/">Click For Live App!</a>
         </Card.Footer>
       </Card>
     </CardGroup>
